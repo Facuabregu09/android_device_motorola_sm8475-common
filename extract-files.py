9c9467da2b1c1035a4df5109de5f2b74026942d2
Facundo Abregu <facuabregu09@gmail.com>
2026-07-01 06:29:33 +0000

    sm8475-common: Add extract-files.py; exclude ST54J NFC and non-hiphi touch variants from proprietary-files
    Change-Id: I4452a6e5f77652dab9b6aa79a9eab0e317cc867e

--- extract-files.py
+++ extract-files.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
 #
-# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
@@ -14,47 +13,55 @@ from extract_utils.fixups_lib import (
     lib_fixups_user_type,
 )
 from extract_utils.main import (
-    ExtractUtils,
     ExtractUtilsModule,
 )
 
 namespace_imports = [
     'device/motorola/sm8475-common',
     'hardware/motorola',
-    'hardware/qcom-caf/common/libqti-perfd-client',
     'hardware/qcom-caf/sm8450',
     'hardware/qcom-caf/wlan',
-    'vendor/qcom/opensource/commonsys-intf/display',
     'vendor/qcom/opensource/commonsys/display',
     'vendor/qcom/opensource/dataservices',
 ]
 
 
+libs_add_vendor_suffix = (
+    'vendor.qti.hardware.qccsyshal@1.0',
+    'vendor.qti.hardware.qccsyshal@1.1',
+    'vendor.qti.qspmhal@1.0',
+    'vendor.qti.imsrtpservice@3.0',
+    'vendor.qti.diaghal@1.0',
+    'com.qualcomm.qti.dpm.api@1.0',
+)
+
+libs_remove = (
+    'libar-pal',
+    'libar-acdb',
+    'liblx-osal',
+    'libats',
+    'libagm',
+    'libpalclient',
+)
+
+
 def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
-    return f'{lib}_{partition}' if partition == 'vendor' else None
+    if partition != 'vendor':
+        return None
+
+    return f'{lib}_{partition}'
 
 
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
-    (
-    'vendor.qti.hardware.qccsyshal@1.0',
-    'vendor.qti.hardware.qccsyshal@1.1',
-    'vendor.qti.imsrtpservice@3.0',
-    'vendor.qti.diaghal@1.0',
-    'vendor.qti.hardware.wifidisplaysession@1.0',
-    'com.qualcomm.qti.dpm.api@1.0',
-    ): lib_fixup_vendor_suffix,
+    libs_add_vendor_suffix: lib_fixup_vendor_suffix,
+    libs_remove: lib_fixup_remove,
 }
+
 
 blob_fixups: blob_fixups_user_type = {
     'system_ext/etc/permissions/moto-telephony.xml': blob_fixup().regex_replace(
         '/system/', '/system_ext/'
-    ),
-    'system_ext/lib64/libwfdnative.so': blob_fixup().add_needed(
-        'libinput_shim.so'
-    ),
-    'system_ext/lib64/vendor.qti.hardware.qccsyshal@1.2-halimpl.so': blob_fixup().replace_needed(
-        'libprotobuf-cpp-full.so', 'libprotobuf-cpp-full-21.7.so'
     ),
     (
         'vendor/bin/hw/android.hardware.security.keymint-service-qti',
@@ -73,15 +80,13 @@ blob_fixups: blob_fixups_user_type = {
         'android.hardware.security.sharedsecret-V1-ndk.so',
     )
     .add_needed('android.hardware.security.rkp-V1-ndk.so'),
-    'vendor/bin/init.kernel.post_boot.sh': blob_fixup().regex_replace(
-        'ro.boot.using_zram_from_fstab', 'ro.vendor.zram.swapon'
+    'vendor/bin/qcc-trd': blob_fixup().replace_needed(
+        'libgrpc++_unsecure.so', 'libgrpc++_unsecure_prebuilt.so'
     ),
-    'vendor/etc/sensors/hals.conf': blob_fixup().add_line_if_missing(
-        'sensors.moto_ext.so',
-    ),
-    'vendor/etc/public.libraries.txt': blob_fixup()
-        .regex_replace('libqti-perfd-client.so\n', ''),
     'vendor/lib64/libmotext_inf.so': blob_fixup().remove_needed('libril.so'),
+#     'system_ext/priv-app/ims/ims.apk': blob_fixup().apktool_patch(
+#         'ims-patches'
+#     ),
     'vendor/lib64/vendor.libdpmframework.so': blob_fixup().add_needed(
         'libhidlbase_shim.so'
     ),
@@ -90,26 +95,16 @@ blob_fixups: blob_fixups_user_type = {
     ),
     'vendor/lib64/sensors.moto.so': blob_fixup().add_needed('libbase_shim.so'),
     (
-        'vendor/etc/media_codecs_waipio_v0.xml',
-        'vendor/etc/media_codecs_waipio_v1.xml',
-        'vendor/etc/media_codecs_waipio_v2.xml',
-        'vendor/etc/media_codecs_ravelin.xml',
-    ): blob_fixup().regex_replace(
-        '.*media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio|dolby_audio).*\n',
-        '',
-    ),
-    (
         'vendor/etc/seccomp_policy/atfwd@2.0.policy',
         'vendor/etc/seccomp_policy/modemManager.policy',
-        'vendor/etc/seccomp_policy/wfdhdcphalservice.policy',
-    ): blob_fixup().add_line_if_missing(
-        'gettid: 1'
-    ),
+        'vendor/etc/seccomp_policy/sensors-qesdk.policy',
+    ): blob_fixup().add_line_if_missing('gettid: 1'),
     (
-        'vendor/lib64/libdpps.so',
-        'vendor/lib64/libsnapdragoncolor-manager.so',
-    ): blob_fixup().replace_needed(
-        'libtinyxml2.so', 'libtinyxml2-v34.so'
+        'vendor/lib64/libqcrilNr.so',
+        'vendor/lib64/libril-db.so',
+    ): blob_fixup().binary_regex_replace(
+        rb'persist\.vendor\.radio\.poweron_opt',
+        b'persist.vendor.radio.poweron_ign',
     ),
 }
 
@@ -120,7 +115,3 @@ module = ExtractUtilsModule(
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
 )
-
-if __name__ == '__main__':
-    utils = ExtractUtils.device(module)
-    utils.run()
